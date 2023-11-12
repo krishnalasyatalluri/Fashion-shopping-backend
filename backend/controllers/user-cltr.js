9ef422029a294f185d0162558edf4c2e8aac1cca
Krishna <krishnalasyatalluri9@gmail.com>
2023-11-12 19:02:27 +0530

    completed all API's

--- backend/controllers/user-cltr.js
+++ backend/controllers/user-cltr.js
@@ -1,12 +1,10 @@
 const pick = require('lodash/pick')
 const User = require('../models/userModel')
-const bcrypt=require('bcryptjs')
-const jwt=require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const userCltr = {}
 userCltr.login = async (req, res) => {
-    const body = pick(req.body, ['customerName', 'username', 'password','preferredCategory'])
-    const hashedPassword=await bcrypt.hash(body.password,10)
-    const user = new User({...body,password:hashedPassword})
+    const body = pick(req.body, ['customerName', 'username', 'password', 'preferredCategory'])
     try {
         const userDoc = await User.findOne({ username: body.username })
         //if username already exists
@@ -15,12 +13,14 @@ userCltr.login = async (req, res) => {
 
         } else {
             //if username doesn't exist 
+            const hashedPassword = await bcrypt.hash(body.password, 10)
+            const user = new User({ ...body, password: hashedPassword })
             const userRecord = await user.save()
-            const token=jwt.sign({username:body.username},'backend')
+            const token = jwt.sign({ username: body.username }, 'backend')
             res.status(200).json({ message: 'credentials inserted successfully' })
         }
     } catch (error) {
-        res.status(500).json({message:'Internal server error'})
+        res.status(500).json({ message: 'Internal server error' })
 
     }
 
