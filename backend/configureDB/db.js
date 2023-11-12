c366d11c1747a3aebb8d9f94598aa9f3101a15dc
Krishna <krishnalasyatalluri9@gmail.com>
2023-11-12 17:59:06 +0530

    made code clean

--- backend/configureDB/db.js
+++ backend/configureDB/db.js
@@ -4,10 +4,6 @@ const configureDB=async()=>{
         const url=process.env.DB_URL||'mongodb://127.0.0.1:27017'
         const dbName=process.env.DB_NAME||'user_creds'
         const db=await mongoose.connect(`${url}/${dbName}` )
-        // ,{
-        //     useNewUrlParser: true,
-        //     useUnifiedTopology: true,
-        // })
         console.log('connected to db')
     }
     catch(error){
